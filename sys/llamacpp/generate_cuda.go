5a9c8a8a1d6c326d53665ad00f59bc8ef4835ab3
David Thorpe <djt@mutablelogic.com>
2026-01-31 14:43:30 +0100

    Small fixes

--- sys/llamacpp/generate_cuda.go
+++ sys/llamacpp/generate_cuda.go
@@ -7,5 +7,6 @@ package llamacpp
 
 /*
 #cgo pkg-config: libllama-cuda cuda-12.6 cublas-12.6 cudart-12.6
+#cgo LDFLAGS: -L/usr/local/cuda/lib64/stubs -L/usr/local/cuda/targets/aarch64-linux/lib/stubs -L/usr/local/cuda/targets/x86_64-linux/lib/stubs
 */
 import "C"
